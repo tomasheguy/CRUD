b9a7bd90c25b46fa2b3da890b44b28b7c52188e0
Tomas Heguy <tomasheguy@Tomass-MacBook-Air.local>
2021-10-30 15:55:49 -0300

    avances sabado al medio dia... va mas o menos

---
+++ src/controllers/mainController.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		// new array w products que cumplen con condicion visited
+		const productsVisited = products.filter((prod) =>  prod.category === "visited");
+		const productsOnSale = products.filter((prod) =>  prod.category === "in-sale");
+		res.render('index',{productsVisited,productsOnSale}); // faltar filtrarlos por un lado en oferta y visitados x otro
+	},
+	search: (req, res) => {
+		res.render('results',{products}); // mandamos todos los productos para despues filtrarlos
+	},
+};
+
+module.exports = controller;
---
+++ src/controllers/productsController.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));// pasa el json a un array
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products', {products}); //le paso el objeto products como parametro
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		const product = products.find((prod) => prod.id == req.params.id);
+		res.render('detail',{product,toThousand});
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		
+		res.render('product-create-form'); //
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		const nuevoProducto = req.body;
+		res.send(nuevoProducto); //no estoy muy seguro de esto
+		res.send('se creo producto');
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		// solo falta auto completar los campos
+		const productToEdit = products.find((prod) => prod.id == req.params.id);
+		res.render('product-edit-form',{productToEdit}); //recibe respueta del formulario
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		const editedProduct = req.body;
+		res.send(editedProduct); //no estoy muy seguro de esto
+		res.send('se actualizo el producto');
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		
+		res.send('se borro el producto ' + req.params.id);
+	}
+};
+
+module.exports = controller;
